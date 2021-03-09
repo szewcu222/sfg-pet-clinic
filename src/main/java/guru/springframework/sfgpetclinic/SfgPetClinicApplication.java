a2c8553c90ac7bbe118146be355e6d01467a20b9
Damian Szewc <szewcu222@gmail.com>
2021-03-09 20:34:54 +0100

    Added index controller and index view

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -10,9 +10,6 @@ public class SfgPetClinicApplication {
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
 
-        OwnerServiceMap sv = new OwnerServiceMap();
-        sv.findAll();
-
     }
 
 
